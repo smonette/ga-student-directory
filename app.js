d8bba296e3125a588b38b5b683c01b60df52619c
smonette <steph@stephmonette.com>
2014-08-05 17:23:27 -0700

    adding authentication, building the edit form, building profile

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     db = require('./models/index.js'),
     bodyParser = require('body-parser'),
+    methodOverride = require("method-override"),
     passport= require("passport"),
     pasportLocal = require("passport-local"),
     cookieParser = require("cookie-parser"),
@@ -11,6 +12,8 @@ var express = require('express'),
     app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(methodOverride("_method"));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}) );
@@ -80,6 +83,11 @@ app.get('/home', function (req, res) {
   }
 });
 
+app.get('/logout', function(req,res){
+  req.logout();
+  res.redirect('/login')
+});
+
 // POSTS FOR SIGN UP, LOGIN & EDIT PROFILE
 
 app.post('/create', function(req,res){
@@ -101,9 +109,82 @@ app.post('/login', passport.authenticate('local', {
   failureFlash: true
 }));
 
+
+app.put('/edit/:id', function(req,res){
+  // find the user by id grab from url
+  // make sure only the user can change their profile 
+
+    db.user.find({
+      where: {
+        id: req.params.id
+      }
+    })
+    .success(function(foundUser){
+      console.log(req.body.bio);
+
+      foundUser.updateAttributes ( {
+        website: req.body.website, 
+        twitterhandle: req.body.twitterhandle, 
+        linkedin: req.body.linkedin, 
+        bio: req.body.bio, 
+        samplework: req.body.addlinfo 
+      } )
+    })
+    .success(function(user){
+      console.log(user)
+      res.redirect('/user/' + Number(req.params.id) );
+    });
+
+
+});
+
 // USER FILES
-app.get('/edit', function (req, res) {
-  res.render('user/edit');
+
+
+
+
+
+
+app.get('/user/:id', function (req, res) {
+
+    db.user.find({
+      where: {
+        id: req.params.id
+      }
+    })
+    .success(function(foundUser) {
+      res.render("user/profile", 
+        { isAuthenticated: req.isAuthenticated(),
+          user: foundUser
+        });
+   });
+
+
+});
+
+
+app.get('/edit/:id', function (req, res) {
+
+  var id = req.params.id;
+  db.user.find(id)
+  .success(function(foundUser){
+    console.log(foundUser.bio);
+
+      res.render('user/edit', 
+        { id: foundUser.id,
+        firstname: foundUser.firstname, 
+        lastname: foundUser.lastname, 
+        email: foundUser.email, 
+        website: foundUser.website || " ", 
+        twitterhandle: foundUser.twitterhandle || "", 
+        linkedin: foundUser.linkedin || "", 
+        bio: foundUser.bio || "", 
+        addlinfo: foundUser.samplework || "", 
+        isAuthenticated: req.isAuthenticated()
+        
+      })
+  })
+
 });
 
 
