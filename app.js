5a264cee8e8a7765da9f229b85c01e84da9d9011
smonette <steph@stephmonette.com>
2014-08-07 17:37:22 -0700

    updating edit button

--- app.js
+++ app.js
@@ -307,33 +307,6 @@ app.get('/*', function (req, res) {
 
 
 
-// Testing!
-
-// var request = require('supertest');
-
-// describe("Routing site files", function(){
-
-//   it ("root should return", function(done){
-//     request(app)
-//       .get('/')
-//       .expect(200)
-//       .end(done);
-//   });
-//   it ("about should return", function(done){
-//     request(app)
-//       .get('/about')
-//       .expect(200)
-//       .end(done);
-//   });
-//   it ("login should return", function(done){
-//     request(app)
-//       .get('/login')
-//       .expect(200)
-//       .end(done);
-//   });
-
-// });
-
 
 
 
