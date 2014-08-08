ddf3d0d8fe5071459d43238c015d82f2b6ba15f3
smonette <steph@stephmonette.com>
2014-08-07 18:43:40 -0700

    console logging

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.get('/user/:id', function (req, res) {
             var url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" + foundUser.twitterhandle + "&count=6";
             retreieveTweets(url, function(allTweets){ 
                   
-                if(allTweets.user){
+                if(allTweets.user[0].protected === false){
 
                   res.render("user/profile", 
                   { isAuthenticated: req.isAuthenticated(),
